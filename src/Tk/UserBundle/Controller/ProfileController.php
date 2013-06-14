d8a66f3539c2bd2254e58a845456efecdbcbcace
JulesM <jules.marcilhacy@gmail.com>
2013-06-14 11:32:47 +0200

    change member names with username

--- src/Tk/UserBundle/Controller/ProfileController.php
+++ src/Tk/UserBundle/Controller/ProfileController.php
@@ -82,6 +82,10 @@ class ProfileController extends Controller
 
             if ($form->isValid()) {
 
+                foreach ($user->getMembers() as $member){
+                    $member->setName($user->getUsername());
+                }
+
                 $em = $this->getDoctrine()->getEntityManager();
                 $em->persist($user);
                 $em->flush();

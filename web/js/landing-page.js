960138d5cc8ab2227f590b9532ebf8d166175393
ArnaudDri <arnaud.drizard@gmail.com>
2013-06-28 19:19:56 +0200

    navbar+mosaic

---
+++ web/js/landing-page.js
@@ -0,0 +1,15 @@
+
+$(document).scroll(function() {
+
+    if( $(this).scrollTop() > 2800 ) {	
+    $('#login-mosaic').fadeIn(1000);
+      $('#login-mosaic').css({display:'block',  position: 'fixed', left:0, top:50});
+    }else if( $(this).scrollTop() < 2400 ) {
+    $('#login-mosaic').css({display:'none',  position: 'fixed', left:0, top:50});
+    }
+    else{
+    $('#login-mosaic').fadeOut(500);
+    }
+
+
+});

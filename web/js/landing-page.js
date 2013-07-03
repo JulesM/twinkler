1274342705aecbc0db9728b314591b47532ab744
ArnaudDri <arnaud.drizard@gmail.com>
2013-07-03 19:48:28 +0200

    no background on expense

--- web/js/landing-page.js
+++ web/js/landing-page.js
@@ -73,3 +73,16 @@ $(document).scroll(function() {
 
 });
 
+
+$(document).ready(function(){
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+        var target = this.hash,
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+});

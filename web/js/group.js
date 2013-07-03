559d2a1a93e4c1cc332ab8d6e402f87b494fea79
ArnaudDri <arnaud.drizard@gmail.com>
2013-07-03 19:07:45 +0200

    expense page - table

--- web/js/group.js
+++ web/js/group.js
@@ -5,3 +5,11 @@
 		$('#content').css('height',$('#page-body').height()-$('#page-header').height());
 	}
 };
+
+ window.onresize =function() {
+	if ($(window).height()>$('#page-body').height())
+	{
+		document.getElementById('page-body').style.height = $(window).height()  + 'px';
+		$('#content').css('height',$('#page-body').height()-$('#page-header').height());
+	}
+};

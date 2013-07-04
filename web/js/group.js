848d6ffbf80e87123db1c91b6914045ae95b2ea5
ArnaudDri <arnaud.drizard@gmail.com>
2013-07-04 16:19:25 +0200

    graph

--- web/js/group.js
+++ web/js/group.js
@@ -1,3 +1,27 @@
+
+var data = {
+	labels : ["Julio","Nono","Tsonga","Guigui"],
+	datasets : [
+		{
+			fillColor :["rgba(249,126,118,0.5)","rgba(168,189,68,0.5)","rgba(249,126,118,0.5)","rgba(168,189,68,0.5)"	],
+			strokeColor : "rgba(220,220,220,1)",
+			data : [-65,20,-90,81]
+		}
+	]
+}
+
+
+
+
+
+var ctx = document.getElementById("balanceChart").getContext("2d");
+new Chart(ctx).Bar(data,{
+    scaleOverlay : false,
+
+	scaleShowLabels : false
+});
+
+
  window.onload =function() {
 	if ($(window).height()>$('#page-body').height())
 	{
@@ -12,4 +36,6 @@
 		document.getElementById('page-body').style.height = $(window).height()  + 'px';
 		$('#content').css('height',$('#page-body').height()-$('#page-header').height());
 	}
+
+
 };

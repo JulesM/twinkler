67ed790d7916a6ab79b4fa901a0f4326e0699888
ArnaudDri <arnaud.drizard@gmail.com>
2013-07-06 14:19:10 +0200

    graph synchro db

--- web/js/group.js
+++ web/js/group.js
@@ -20,19 +20,33 @@
 
 /*--------CHARTS--------*/
 
-var data = {
-	labels : ["Julio","Nono","Tsonga","Guigui"],
-	datasets : [
-		{
-			fillColor :["rgba(249,126,118,0.5)","rgba(168,189,68,0.5)","rgba(249,126,118,0.5)","rgba(168,189,68,0.5)"	],
-			strokeColor : "rgba(220,220,220,1)",
-			data : [-65,20,-90,81]
+var graphColor=function(graphData){
+	var chartColor=[];
+	for(var i=0; i<graphData.length;i++){
+		if(graphData[i]>=0){
+			chartColor.push("rgba(168,189,68,0.5)");
+		}else{			
+			chartColor.push("rgba(249,126,118,0.5)");
 		}
-	]
+	};
+	return chartColor
 }
 
 
+var colorFill=graphColor(balances);
 
+
+
+				var data = {
+							labels : members,
+							datasets : [
+										{
+											fillColor : colorFill,
+											strokeColor : "rgba(220,220,220,1)",
+											data : balances
+										}
+										]
+									}
 
 
 var ctx = document.getElementById("balanceChart").getContext("2d");

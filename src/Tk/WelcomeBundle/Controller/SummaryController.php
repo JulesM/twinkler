b21331ae0e7590e02eb86681b0932c72f2435123
JulesM <jules.marcilhacy@gmail.com>
2013-07-11 09:58:01 +0200

    summary

--- src/Tk/WelcomeBundle/Controller/SummaryController.php
+++ src/Tk/WelcomeBundle/Controller/SummaryController.php
@@ -14,49 +14,14 @@ class SummaryController extends Controller
         if ($token != $group->getInvitationToken()){
             throw new AccessDeniedException('You try to access a wrong Url');
         }else{
+            $expenses_service = $this->container->get('tk_expense.expenses');
+
             return $this->render('TkWelcomeBundle:Invitation:summary.html.twig', array(
                 'group'               => $group,
-                'all_expenses'        => $this->getAllExpensesAction(),
-                'total_paid'          => $this->getTotalPaidAction(),
-                'total_paid_by_me'    => $this->getTotalPaidByMeAction(),
-                'total_paid_supposed' => $this->getTotalSupposedPaidAction(),
-                'debts'               => $this->getCurrentDebtsAction(),
+                'all_expenses'        => $group->getExpenses(),
+                'total_paid'          => $expenses_service->getTotalPaid($group),
+                'debts'               => $expenses_service->getCurrentDebts($group),
                 ));
         }
-    }
-
-    private function getAllExpensesAction()
-    {
-        $member = $this->getUser()->getCurrentMember();
-        $expenses_service = $this->container->get('tk_expense.expenses');
-        return $expenses_service->getAllExpenses($member);
-    }
-
-    private function getTotalPaidAction()
-    {
-        $member = $this->getUser()->getCurrentMember();
-        $expenses_service = $this->container->get('tk_expense.expenses');
-        return $expenses_service->getTotalPaid($member->getTGroup());
-    }
-
-    private function getTotalPaidByMeAction()
-    {
-        $member = $this->getUser()->getCurrentMember();
-        $expenses_service = $this->container->get('tk_expense.expenses');
-        return $expenses_service->getTotalPaidByMe($member);
-    }
-
-    private function getTotalSupposedPaidAction()
-    {
-        $member = $this->getUser()->getCurrentMember();
-        $expenses_service = $this->container->get('tk_expense.expenses');
-        return $expenses_service->getTotalSupposedPaid($member);
-    }
-
-    private function getCurrentDebtsAction()
-    {
-        $member = $this->getUser()->getCurrentMember();
-        $expenses_service = $this->container->get('tk_expense.expenses');
-        return $expenses_service->getCurrentDebts($member->getTGroup());
     }
 }
